b050a9ed15651f13af0c298443beb063fb534852
Franz Wöllert <franz.woellert@gmail.com>
2016-10-04 00:40:08 +0200

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='confipy',
+    version='0.0.1',
+    packages=find_packages(),
+    url='https://github.com/mansenfranzen/confipy',
+    license='MIT',
+    author='Franz Woellert',
+    author_email='franz.woellert@gmail.com',
+    description='A convenient config file reader.',
+    install_requires=['PyYAML']
+)
