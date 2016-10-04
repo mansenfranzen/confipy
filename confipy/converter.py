4b616fe91ca34fb2ca81cdf07e226968667f8eac
Franz Wöllert <franz.woellert@gmail.com>
2016-10-04 20:21:38 +0200

    Remove obsolete get() method which is substituted via the __call__("get") method in order to prevent namespace clashes. Add doc strings. Add try-except for _unflat_dict to account for dict and DotNotation config types.

--- confipy/converter.py
+++ confipy/converter.py
@@ -1,22 +1,61 @@
 """This module contains converter functions."""
 
 class DotNotation(object):
-    """Enables dot notation for accessing config properties."""
+    """Enables dot notation for accessing config properties. This class does
+    not inherit from dict on purpose in order to prevent namespace clashes.
+    Therefore, only operator overloading methods are used which are unlikely
+    to clash with variable names from config files.
 
-    def get(self, key, default=None):
-        if hasattr(self, key):
-            return getattr(self, key)
-        return default
+    """
 
     def __setitem__(self, key, value):
+        """Support bracketing attribute setting."""
         setattr(self, key, value)
 
+    def __call__(self, ret="val", key=None, default=None):
+        """Return specific attributes of the DotNotation instance.
+
+        Parameters
+        ----------
+        ret: {"val", "dot", "dict", "get"}
+            Define the return value. 'val' refers to attributes which are not
+            DotNotation instances (therefore being str/lists by default).
+            'dot' refers to attributes which are DotNotation instances. 'dict'
+            provides the possibility to return itself as a dictionary. 'get'
+            mimics the default dict.get() method. It returns the attribute if
+            present. Otherwise returns the 'default' parameter.
+
+        """
+
+        if ret == "val":
+            return {key: value for key, value in vars(self).items()
+                    if not isinstance(value, DotNotation)}
+
+        elif ret == "dot":
+            return {key: value for key, value in vars(self).items()
+                    if isinstance(value, DotNotation)}
+
+        elif ret == "dict":
+            res_dict = {}
+            for key, value in vars(self).items():
+                if isinstance(value, self.__class__):
+                    res_dict[key] = value("dict")
+                    continue
+                res_dict[key] = value
+            return res_dict
+
+        elif ret == "get":
+            if hasattr(self, key):
+                return getattr(self, key)
+            else:
+                return default
+
+
     def __repr__(self):
+        """Return string representation."""
         tpl = "CfgNode: {} nodes ({}) / {} values ({})"
-        nodes = [x for x in vars(self)
-                 if isinstance(getattr(self,x), DotNotation)]
-        values = [x for x in vars(self)
-                  if not isinstance(getattr(self,x), DotNotation)]
+        nodes = self("dot").keys()
+        values = self("val").keys()
         return tpl.format(len(nodes), nodes, len(values), values)
 
 
@@ -35,7 +74,7 @@ def _unflat_dict(flat_dict, unflat_dict=None, default_type=dict):
     unflattend_dict: dict
 
     """
-
+    print(flat_dict)
     if not unflat_dict:
         unflat_dict = default_type()
 
@@ -49,7 +88,11 @@ def _unflat_dict(flat_dict, unflat_dict=None, default_type=dict):
             continue
 
         # if key exists, do not create again
-        key_exists = unflat_dict.get(this_key)
+        try:
+            key_exists = unflat_dict.get(this_key)
+        except AttributeError:
+            key_exists = unflat_dict("get", this_key)
+
         if key_exists:
             _unflat_dict({key_chain[1:]: value}, key_exists, default_type)
             continue
