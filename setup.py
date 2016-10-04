74998cc2667df0e9f429bac11608e98adcd8487e
Franz Wöllert <franz.woellert@gmail.com>
2016-10-05 00:27:11 +0200

    New setup.

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+import sys
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
 
 setup(
     name='confipy',
     version='0.0.1',
-    packages=find_packages(),
+    packages=['confipy'],
     url='https://github.com/mansenfranzen/confipy',
     license='MIT',
     author='Franz Woellert',
@@ -11,5 +26,7 @@ setup(
     description='A convenient config file reader.',
     install_requires=['PyYAML', 'six'],
     download_url = 'https://github.com/mansenfranzen/confipy/tarball/0.0.1',
-    keywords = ['config', 'yaml']
+    keywords = ['config', 'yaml'],
+    tests_require=['pytest'],
+    cmdclass={'test': PyTest}
 )
